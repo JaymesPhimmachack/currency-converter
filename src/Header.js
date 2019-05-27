2dc1d5c07532ab8794c36290adc5ae1264d2902f
Jaymes Phimmachack <gsxrx750@gmail.com>
2019-05-27 02:29:03 +0000

    Replace class with className

--- src/Header.js
+++ src/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     return (
         <div className="row">
             <div className="col-12">
-            <span class="mr-2"><FaRandom /></span><Link to="/">FX Change</Link><span className="mx-3">Currency Converter</span>
+            <span className="mr-2"><FaRandom /></span><Link to="/">FX Change</Link><span className="mx-3">Currency Converter</span>
             <div>
                 <Link to="/">Home</Link>
                 <Link to="/currency-calculator" className="ml-3">Currency Calculator</Link>
