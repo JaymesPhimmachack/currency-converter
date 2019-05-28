258c8ef211e9b9951a64e7bcd70434f30dd6de97
Jaymes Phimmachack <gsxrx750@gmail.com>
2019-05-28 16:17:24 +0000

    Move bootstrap import

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Footer from './Footer';
 import ExchangeRate from './ExchangeRate';
 import CurrencyCalculator from './CurrencyCalculator';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.css';
+
 
 const App = () => {
 	return (
