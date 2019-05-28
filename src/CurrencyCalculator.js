49bb20c598dab5e44c86db6d45069d6936bb1a7f
Jaymes Phimmachack <gsxrx750@gmail.com>
2019-05-28 18:53:59 +0000

    Add chart to app

--- src/CurrencyCalculator.js
+++ src/CurrencyCalculator.js
@@ -1,38 +1,46 @@
 import React from 'react';
 import CurrencyQuery from './CurrencyQuery';
+import Chart from 'chart.js';
 /* global fetch */
+/* global URLSearchParams*/
 
 class CurrencyCalculator extends React.Component {
 	constructor(props) {
 		super(props);
+		const params = new URLSearchParams(props.location.search);
+		
 		this.state = {
 			isLoading: true,
 			unit: 1,
-			baseCurrency: 'USD',
-			crossCurrency: 'EUR',
+			baseCurrency: params.get('baseCurrency') || 'USD',
+			crossCurrency: params.get('crossCurrency') || 'EUR',
 			rate: 0.89
 		};
 
+    this.chartRef = React.createRef();
 		this.handleInputChange = this.handleInputChange.bind(this);
 		this.handleFirstSelectChange = this.handleFirstSelectChange.bind(this);
 		this.handleSecondSelectChange = this.handleSecondSelectChange.bind(this);
 		this.fetchCurrency = this.fetchCurrency.bind(this);
+		this.getHistoricalRates = this.getHistoricalRates.bind(this);
+		this.buildChart = this.buildChart.bind(this);
 	}
 
 	componentDidMount() {
-		this.fetchCurrency();
+		this.fetchCurrency(this.state.baseCurrency, this.state.crossCurrency);
+		this.getHistoricalRates(this.state.baseCurrency, this.state.crossCurrency);
 	}
 
-	fetchCurrency() {
+	fetchCurrency(baseCurrency, crossCurrency) {
 		fetch(
-			`https://alt-exchange-rate.herokuapp.com/latest?base=${this.state.baseCurrency}&symbols=${this.state
-				.crossCurrency}`
+			`https://alt-exchange-rate.herokuapp.com/latest?base=${baseCurrency}&symbols=${crossCurrency}`
 		)
 			.then((response) => {
 				return response.json();
 			})
 			.then((data) => {
-					this.setState({ isLoading: false, rate: data.rates[this.state.crossCurrency] });
+				console.log(crossCurrency,  data.rates)
+					this.setState({ isLoading: false, rate: data.rates[crossCurrency] });
 			})
 			.catch((error) => {
 				console.log(error);
@@ -45,40 +53,99 @@ class CurrencyCalculator extends React.Component {
 
 	handleFirstSelectChange(baseCurrency) {
 		this.setState({ baseCurrency });
-		this.fetchCurrency();
+		this.fetchCurrency(baseCurrency, this.state.crossCurrency);
+		this.getHistoricalRates(baseCurrency, this.state.crossCurrency);
 	}
 
 	handleSecondSelectChange(crossCurrency) {
 		this.setState({ crossCurrency });
-		this.fetchCurrency();
+		this.fetchCurrency(this.state.baseCurrency, crossCurrency);
+		this.getHistoricalRates(this.state.baseCurrency, crossCurrency);
 	}
-
+	
+	getHistoricalRates(baseCurrency, crossCurrency) {
+		const endDate = new Date().toISOString().split('T')[0];
+		const startDate = new Date((new Date()).getTime() - (30 * 24 * 60 * 60 * 1000)).toISOString().split('T')[0];
+		
+		fetch(
+			`https://alt-exchange-rate.herokuapp.com/history?start_at=${startDate}&end_at=${endDate}&base=${baseCurrency}&symbols=${crossCurrency}`
+		)
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+					if (data.error) {
+						throw new Error(data.error);
+					}
+					
+					const chartLabels = Object.keys(data.rates);
+					const chartData = Object.values(data.rates).map(rate => rate[crossCurrency]);
+					const chartLabel = `${baseCurrency}/${crossCurrency}`;
+					
+					this.buildChart(chartLabels, chartData, chartLabel);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}
+	
+	buildChart (labels, data, label) {
+		const chartRef = this.chartRef.current.getContext('2d');
+		
+		if(typeof this.chart !== 'undefined') {
+			this.chart.destroy();
+		}
+		
+		this.chart = new Chart(chartRef, {
+			type: 'line',
+			data: {
+				labels,
+				datasets: [
+				{
+					label: label,
+					data,
+					fill: false,
+					tension: 0
+				}
+				]
+			},
+			options: {
+				responsive: true
+			}
+		})
+	}
+	
 	render() {
 		return (
-			<div className="row">
-				<h3 className="text-center col-12">Currency Calculator</h3>
-
-				<div className="col-xs-12 col-md-4 mt-4">
-					<CurrencyQuery
-						unit={this.state.unit}
-						baseCurrency={this.state.baseCurrency}
-						crossCurrency={this.state.crossCurrency}
-						handleInputChange={this.handleInputChange}
-						handleFirstSelectChange={this.handleFirstSelectChange}
-						handleSecondSelectChange={this.handleSecondSelectChange}
-					/>
+			<React.Fragment>
+				<div className="row">
+					<h3 className="text-center col-12">Currency Calculator</h3>
+	
+					<div className="col-xs-12 col-md-5 mt-4 mb-3 mx-auto">
+						<CurrencyQuery
+							unit={this.state.unit}
+							baseCurrency={this.state.baseCurrency}
+							crossCurrency={this.state.crossCurrency}
+							handleInputChange={this.handleInputChange}
+							handleFirstSelectChange={this.handleFirstSelectChange}
+							handleSecondSelectChange={this.handleSecondSelectChange}
+						/>
+					</div>
+					<div className="col-xs-12 col-md-5 mt-4 mb-3 mx-auto">
+						<h6>Currency Rate</h6>
+						<span>
+							{this.state.unit} {this.state.baseCurrency}
+						</span>{' '}
+						={' '}
+						<span>
+							{this.state.crossCurrency} {(this.state.rate * this.state.unit).toFixed(4)}
+						</span>
+					</div>
 				</div>
-				<div className="col-xs-12 col-md-8 mt-4 vh-100">
-					<h6>Currency Rate</h6>
-					<span>
-						{this.state.unit} {this.state.baseCurrency}
-					</span>{' '}
-					={' '}
-					<span>
-						{this.state.crossCurrency} {(this.state.rate * this.state.unit).toFixed(4)}
-					</span>
+				<div className="row mb-5">
+					<canvas ref={this.chartRef} />
 				</div>
-			</div>
+			</React.Fragment>
 		);
 	}
 }
